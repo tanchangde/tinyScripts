4b2dbdfaac1e75cb24100ad8f3447d4037857d51
Changde Tan <tanchangde@gmail.com>
2022-06-05 15:36:03 +0800

    style: Some detail adjustments

--- Altmetric/getAltmetricScore.R
+++ Altmetric/getAltmetricScore.R
@@ -18,17 +18,17 @@ if( length(pkgsToInstall) > 0){
 	for(lib in pkgsToInstall) install.packages(lib)
 }
 
-sapply(pkgsToLoad, require, character.only=TRUE)
+sapply(pkgsToLoad, require, character.only =T RUE)
 
 # 构造接口查询函数
 
 getAltmetricScore <- function(doi, api = "https://api.altmetric.com/v1/doi/") {
-	sleepTime <-  runif(n= 1, min = 0, max = 1)
+	sleepTime <-  runif(n = 1, min = 0, max = 1)
 	cat("\n[🐶友好调用] 划水 ", round(sleepTime, digits = 2), " 秒")
 	Sys.sleep(sleepTime)
 	cat("\n[🔍开始查询]", doi, "的 Altmetric Score...")
 	tryCatch({
-	score <- httpGET(paste0(api,doi)) %>% 
+	score <- httpGET(paste0(api, doi)) %>% 
 			parse_json() %>% 
 			extract2("score")
 	cat("\n[🍒查询结果] ")
@@ -44,21 +44,21 @@ getAltmetricScore <- function(doi, api = "https://api.altmetric.com/v1/doi/") {
 
 # 创建查询结果文件夹
 
-pathToResult <- paste0(getwd(),"/queryResult")
+pathToResult <- paste0(getwd(), "/queryResult")
 cat("查询结果将保存至", pathToResult)
 
 if (!"queryResult" %in% dir_ls()) {
 	dir_create(path = "queryResult")
-	toQuery <- readr::read_csv("toQuery.csv", show_col_types = FALSE) %>% 
+	toQuery <- read_csv("toQuery.csv", show_col_types = FALSE) %>% 
 		extract2(1)
 } else if(length(dir_ls(pathToResult)) == 0){
-	toQuery <- readr::read_csv("toQuery.csv", show_col_types = FALSE) %>% 
+	toQuery <- read_csv("toQuery.csv", show_col_types = FALSE) %>% 
 		extract2(1)
 } else {
 	historyQuery <- dir_ls(pathToResult) %>%  # 遍历结果文件夹文件并合并
 		map_dfr(read_csv, show_col_types = FALSE) %>% 
 		distinct()
-	toQuery <- readr::read_csv("toQuery.csv", show_col_types = FALSE) %>% 
+	toQuery <- read_csv("toQuery.csv", show_col_types = FALSE) %>% 
 		left_join(historyQuery, by = c("DOI" = "doiNum")) %>% 
 		filter(is.na(isQuery)) %>% # 剔除已查询条目
 		extract2(1)
@@ -68,14 +68,14 @@ if (length(toQuery) > 0){
 	for (doiNum in toQuery) {
 		tmpResult <-  getAltmetricScore(doi = doiNum)
 		tmpTable <- tibble(doiNum, tmpResult, isQuery = 1)
-		tmpFileName <- paste0((str_extract_all(string = doiNum, pattern = "\\w")) [[1]],collapse = "")
-		write.csv(tmpTable,file = paste0(pathToResult,"/", tmpFileName,".csv"), row.names = FALSE, na = "")
+		tmpFileName <- paste0((str_extract_all(string = doiNum, pattern = "\\w")) [[1]], collapse = "")
+		write.csv(tmpTable, file = paste0(pathToResult,"/", tmpFileName,".csv"), row.names = FALSE, na = "")
 		cat("\n查询结果已保存。\n")
 	}
 	updateQuery <- dir_ls(pathToResult) %>%  # 遍历结果文件夹文件并合并
 		map_dfr(read_csv, show_col_types = FALSE) %>% 
 		distinct() %>% 
-		rename(DOI = doiNum,AltmetricScore = tmpResult) %>% 
+		rename(DOI = doiNum, AltmetricScore = tmpResult) %>% 
 		select(DOI, AltmetricScore)
 	write.csv(updateQuery, file = paste0(getwd(),"/","queryResult.csv"), row.names = FALSE, na = "")
 	} else {
